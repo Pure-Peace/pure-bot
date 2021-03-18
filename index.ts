4a9f11e80e552e62b8e6cd81a9bd1f305f9c80e7
Pure-Peace <940857703@qq.com>
2021-03-19 05:28:38 +0800

    fix: module.exports => export

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import { CqApi } from './src/cq_api';
 import { MessageContext } from './src/context';
 import * as utils from './src/utils';
 
-module.exports = {
+export {
     QQbot,
     BotManager,
     CqApi,
