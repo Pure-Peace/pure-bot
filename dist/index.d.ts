682bade2f956f0e308a78c301e9ef32029226f10
Pure-Peace <940857703@qq.com>
2021-03-20 01:12:50 +0800

    modify: github repo no longer push dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { QQbot } from './src/qqbot';
-import { BotManager } from './src/manager';
-import { CqApi } from './src/cq_api';
-import { MessageContext } from './src/context';
-import * as utils from './src/utils';
-export { QQbot, BotManager, CqApi, MessageContext, utils };
-//# sourceMappingURL=index.d.ts.map
--- dist/src/utils/banner.d.ts
+++
@@ -1,3 +0,0 @@
-declare function showBanner(options?: any, showMemoryUsage?: boolean): void;
-export { showBanner };
-//# sourceMappingURL=banner.d.ts.map
