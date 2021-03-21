113e5e8c3807e6a6755a4b3067e9aa47102ae121
arily <arily@arily.moe>
2021-03-21 17:39:20 +0900

    typed ctx

--- packages/what-ctx-should-looks-like/struct.ts
+++ packages/what-ctx-should-looks-like/struct.ts
@@ -1,16 +1,52 @@
-type SendMessage = string | {
-    content: string,
-    at: string,
-    quote: string,
-    file: string,
-    image: string,
+type MessageSegment = {
+    id?: string,
+    text?: string,
+    notify?: string,
+    quote?: string,
+    file?: string,
+    image?: string,
+    toString?: () => string;
+}
+type Message = string | MessageSegment
+
+type MessageContextMessage = MessageSegment & {
+    id: string,
+    text?: string,
+    segments: [MessageSegment] | {
+        get: () => [MessageSegment]
+    },
+    sender: {
+        name?: string,
+        id: string
+    }
+}
+
+type PlatformType = 'onebot'
+type MessageType = 'privateMessage' | 'publicMessage' | 'channelMessage' | 'groupMessage'
+
+type TypedMessageContext = Partial<Record<MessageType, MessageContextMessage>>
+
+type PlatformContext = Partial<Record<PlatformType, TypedMessageContext>>
+
+type MessageContext = PlatformContext & {
+    platform: string,
+    message: MessageContextMessage
+    quote: (msg: Message | [Message]) => Promise<void>,
+    send: (msg: Message | [Message]) => Promise<void>
 }
 
 const MessageEvent = {
     platform: 'onebot',
     message: {
         id: '123456',
-        content: 'suitable message type!',
+        text: 'suitable message type!',
+        segments: {
+            get () {
+                return [{
+                    text: MessageEvent.message.text
+                } as Message];
+            }
+        },
         sender: {
             name: 'ari',
             id: {
@@ -24,34 +60,34 @@ const MessageEvent = {
             }
         }
     },
-    send (message: string | SendMessage | [SendMessage]) {
+    send (message: string | Message | [Message]) {
         if (Array.isArray(message)) return message.map(m => this.send(m));
-        if (typeof message === 'string') return this.send({ content: message });
+        if (typeof message === 'string') return this.send({ text: message });
         if (message.quote) console.log('quote to messageId', message.quote);
-        if (message.at) console.log('at', message.at);
-        console.log('sending message:', message.content);
+        if (message.notify) console.log('notify', message.notify);
+        console.log('sending message:', message.text);
         console.log('======');
     },
-    quote (quote: string | SendMessage | [SendMessage]) {
+    quote (quote: string | Message | [Message]) {
         if (Array.isArray(quote)) return quote.map(m => this.send(m));
-        if (typeof quote === 'string') return this.quote({ content: quote });
+        if (typeof quote === 'string') return this.quote({ text: quote });
         return this.send({
             quote: this.message.id,
             ...quote
         });
     }
-} as Partial<any>;
+} as unknown as MessageContext;
 
 const PublicMessageEvent = Object.assign({
     get publicMessage () {
         return this.message;
     }
-}, MessageEvent);
+}, MessageEvent) as unknown as MessageContext;
 const GroupMessageEvent = Object.assign({
     get groupMessage () {
         return this.message;
     }
-}, PublicMessageEvent);
+}, PublicMessageEvent) as unknown as MessageContext;
 const OnebotGroupMessageEvent = Object.assign({
     get groupMessage () {
         return this.message;
@@ -59,14 +95,14 @@ const OnebotGroupMessageEvent = Object.assign({
     get onebot () {
         return this;
     }
-}, PublicMessageEvent);
+}, PublicMessageEvent) as unknown as MessageContext;
 
 console.log(GroupMessageEvent.message === GroupMessageEvent.onebot?.groupMessage);
 console.log(OnebotGroupMessageEvent.message === OnebotGroupMessageEvent.onebot?.groupMessage);
 console.log();
 GroupMessageEvent.quote('quote to message');
 GroupMessageEvent.send({
-    at: GroupMessageEvent.message.sender.id.toString(),
-    content: 'at sender'
+    notify: GroupMessageEvent.message.sender.id.toString(),
+    text: 'notify sender'
 });
 GroupMessageEvent.send('a quick message');
